59b1f268d606e7a0c5b3ccac84681c855147b976
iv <dokaperi7@gmail.com>
2023-03-06 18:25:50 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Camera } from './Camera/Camera';
+export const App: React.FC = () => (
+  <><h1>Hello React</h1><Camera /></>
+);
---
+++ src/Camera/Camera.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Camera: React.FC = () => (
+    <h1>This is camera</h1>
+);
