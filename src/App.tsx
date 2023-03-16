cd2571c3b70cea04af1897fc86e0352683b4657c
iv <dokaperi7@gmail.com>
2023-03-16 09:11:51 +0100

    Setting Up project

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Camera } from './Camera/Camera';
-export const App: React.FC = () => (
-  <><h1>Hello React</h1><Camera /></>
-);
--- src/Camera/Camera.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const Camera: React.FC = () => (
-    <h1>This is camera</h1>
-);
