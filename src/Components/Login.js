bf9a9cbc2c1205e0eeee6df03010983e06101cde
iv <dokaperi7@gmail.com>
2023-03-29 10:09:52 +0200

    Implementing state managment

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 function Home() {
-    return (
-      <div className="App">
-          <header className="App-header">
-              Login page
-              <Link to="/">Back to home</Link>
-        </header>
-      </div>
-  
-    );
-  }
-  
-  export default Home;
-  
+  const dispatch = useDispatch();
+  const decrement = () => dispatch({ type: "DECREMENT" });
+  const increment = () => dispatch({ type: "INCREMENT" });
+
+  const counter = useSelector((state) => state.count);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Login page
+        <Link to="/">Back to home</Link>
+        <h1>Count: {counter}</h1>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
